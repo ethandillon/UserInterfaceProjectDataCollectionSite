c2861116e0494be3ef4f1273785a8289b29525b6
Ethan Dillon <ethanbradforddillon@gmail.com>
2025-09-30 13:00:05 -0400

    Fix css issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import UserInputForm from './components/UserInputForm'
 import MovieSelection from './components/MovieSelection'
-import './App.css'
 
 function App() {
   const [userData, setUserData] = useState(null)
@@ -12,27 +11,25 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Routes>
-        <Route 
-          path="/" 
-          element={
-            userData ? 
-              <Navigate to="/movies" replace /> : 
-              <UserInputForm onUserSubmit={handleUserSubmit} />
-          } 
-        />
-        <Route 
-          path="/movies" 
-          element={
-            userData ? 
-              <MovieSelection userData={userData} /> : 
-              <Navigate to="/" replace />
-          } 
-        />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route 
+        path="/" 
+        element={
+          userData ? 
+            <Navigate to="/movies" replace /> : 
+            <UserInputForm onUserSubmit={handleUserSubmit} />
+        } 
+      />
+      <Route 
+        path="/movies" 
+        element={
+          userData ? 
+            <MovieSelection userData={userData} /> : 
+            <Navigate to="/" replace />
+        } 
+      />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   )
 }
 
